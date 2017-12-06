f13df465781d47564d346d342f9664112c858df3
セロラー <celelyer@serora-no-macbook-pro-52153.local>
2017-12-06 21:11:05 +0900

    スタート画面作成

--- BoundBeans/GameScene.swift
+++ BoundBeans/GameScene.swift
@@ -7,17 +7,17 @@
 //
 
 import SpriteKit
+import UIKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var myButton: UIButton!
     
     var mame:SKSpriteNode!
     var backScreenNode:SKSpriteNode!
     var Leaf:SKSpriteNode!
-    var Leaf2:SKSpriteNode!
     var ki:SKSpriteNode!
-    var ki2:SKSpriteNode!
     var scrollNode:SKNode!
-    var scrollNode2:SKNode!
     
     //衝突判定カテゴリー
     let mameCategory: UInt32 = 1 << 0
@@ -27,25 +27,72 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     //SKView上にシーンが表示された時に呼ばれるメソッド
     override func didMove(to view: SKView) {
+        // Buttonを生成する.
+        myButton = UIButton()
+        
+        // ボタンのサイズ.
+        let bWidth: CGFloat = 200
+        let bHeight: CGFloat = 50
+        
+        // ボタンのX,Y座標.
+        let posX: CGFloat = self.view!.frame.width/2 - bWidth/2
+        let posY: CGFloat = self.view!.frame.height/2 - bHeight/2
+        
+        // ボタンの設置座標とサイズを設定する.
+        myButton.frame = CGRect(x: posX, y: posY, width: bWidth, height: bHeight)
+        
+        // ボタンの背景色を設定.
+        myButton.backgroundColor = UIColor.red
+        
+        // ボタンの枠を丸くする.
+        myButton.layer.masksToBounds = true
+        
+        // コーナーの半径を設定する.
+        myButton.layer.cornerRadius = 20.0
+        
+        // タイトルを設定する(通常時).
+        myButton.setTitle("Start", for: .normal)
+        myButton.setTitleColor(UIColor.white, for: .normal)
+        
+        // タイトルを設定する(ボタンがハイライトされた時).
+        //myButton.setTitle("ボタン(押された時)", for: .highlighted)
+        //myButton.setTitleColor(UIColor.black, for: .highlighted)
+        
+        // ボタンにタグをつける.
+        myButton.tag = 1
+        
+        // イベントを追加する
+        myButton.addTarget(self, action: #selector(onClickMyButton(sender:)), for: .touchUpInside)
+        
+        // ボタンをViewに追加.
+        self.view!.addSubview(myButton)
+        
+    }
+    
+    @objc func onClickMyButton(sender : UIButton){
+        myButton.removeFromSuperview()
+        
         print("Start")
         //重力を設定
         physicsWorld.gravity = CGVector(dx: 0.0, dy: 9.8)
         physicsWorld.contactDelegate = self
         
         
-        
         //背景色を設定
-        //backgroundColor = UIColor(colorLiteralRed: 0.15, green: 0.75, blue: 0.90, alpha: 1)
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        backScreenNode = SKSpriteNode()
+        backScreenNode.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        backScreenNode.physicsBody = SKPhysicsBody(circleOfRadius: 1.0)
+        backScreenNode.physicsBody?.mass = 0.07
+        backScreenNode.physicsBody?.isDynamic = true
+        backScreenNode.physicsBody?.allowsRotation = false
+        addChild(backScreenNode)
         
         setupKi()
         setupLeaf()
         setupBean()
-        
-        //setKi()
-        //setLeaf()
-        
     }
+    
     
     //画面をタップした時に呼ばれるメソッド
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -118,6 +165,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //スプライトの位置を指定する
         //Leaf.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 3)
         Leaf.position = CGPoint(x: 0.0, y: 0.0)
+        Leaf.zPosition = 100
         
         //物理演算を設定
         Leaf.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: leafTexture.size().width, height: leafTexture.size().height / 10))
@@ -137,112 +185,57 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
         //スプライトを追加する
-        ki.addChild(Leaf)
+        backScreenNode.addChild(Leaf)
     }
     
-    func setLeaf(){ //画面外に設置する
-        //葉の画像を読み込む
-        let leafTexture = SKTexture(imageNamed: "Leaf_1")
-        leafTexture.filteringMode = SKTextureFilteringMode.linear
-        
-        //テクスチャを指定してスプライトを作成する
-        Leaf2 = SKSpriteNode(texture: leafTexture)
-        
-        //スプライトの位置を指定する
-        Leaf2.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 3 - self.frame.size.height)
-        
-        //物理演算を設定
-        Leaf2.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: leafTexture.size().width, height: leafTexture.size().height / 10))
-        Leaf2.physicsBody?.categoryBitMask = leafCategory
-        Leaf2.physicsBody?.contactTestBitMask = mameCategory
-        
-        
-        //衝突の時に動かないようにする
-        Leaf2.physicsBody?.isDynamic = true
-        //衝突の時に回らないようにする
-        Leaf2.physicsBody?.allowsRotation = false
-        //質量を１に設定(木と同じ)
-        Leaf2.physicsBody?.mass = 0.0
-        
-        //親ノードにピン留(親ノードの中心座標を原点とする)
-        Leaf2.physicsBody?.pinned = false
-        
-        
-        //スプライトを追加する
-        addChild(Leaf2)
-    }
+
     
     func setupKi(){
         //豆の木の画像を読み込む
         let kiTexture = SKTexture(imageNamed: "mamenoki")
         kiTexture.filteringMode = SKTextureFilteringMode.nearest
         
+        //必要な枚数を計算
+        let needNumber = 2.0 + (frame.size.height / kiTexture.size().height)
+        
         //スプライトを作成
-        ki = SKSpriteNode(texture: kiTexture)
+        stride(from: 0.0, to: needNumber, by: 1.0).forEach { i in
+            ki = SKSpriteNode(texture: kiTexture)
+            ki.position = CGPoint(x: 0.0, y: -i * ki.size.height)
+            ki.zPosition = -100
+            
+            //物理演算を設定
+            ki.physicsBody = SKPhysicsBody(rectangleOf: kiTexture.size(), center: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height))
+            //質量を設定(葉と同じ)
+            ki.physicsBody?.mass = 0.00001
+            
+            ki.physicsBody?.allowsRotation = false //回転しないようにする
+            ki.physicsBody?.pinned = true
+            
+            backScreenNode.addChild(ki)
+            
+            //次の画面を用意するための判定
+            scrollNode = SKNode()
+            //scrollNode.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+            scrollNode.position = CGPoint(x: 0.0, y: -kiTexture.size().height * 0.25)
+            scrollNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width * 2, height: 1.0))
+            scrollNode.physicsBody?.categoryBitMask = self.scrollCategory
+            scrollNode.physicsBody?.contactTestBitMask = self.mameCategory
+            scrollNode.physicsBody?.mass = 0.00001
+            
+            //ピン留
+            scrollNode.physicsBody?.pinned = true
+            
+            //シーンにスプライトを追加
+            backScreenNode.addChild(scrollNode)
+        }
+
         
         
-        //スプライトの位置を指定する
-        ki.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
-        ki.zPosition = -100
         
-        //物理演算を設定
-        ki.physicsBody = SKPhysicsBody(rectangleOf: kiTexture.size(), center: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height))
-        //質量を設定(葉と同じ)
-        ki.physicsBody?.mass = 0.07
-        
-        ki.physicsBody?.allowsRotation = false //回転しないようにする
-        
-        //次の画面を用意するための判定
-        scrollNode = SKNode()
-        //scrollNode.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
-        scrollNode.position = CGPoint(x: 0.0, y: -kiTexture.size().height * 0.25)
-        scrollNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width * 2, height: 1.0))
-        scrollNode.physicsBody?.categoryBitMask = self.scrollCategory
-        scrollNode.physicsBody?.contactTestBitMask = self.mameCategory
-        scrollNode.physicsBody?.mass = 0.0001
-        
-        //ピン留
-        scrollNode.physicsBody?.pinned = true
-        
-        //シーンにスプライトを追加
-        addChild(ki)
-        ki.addChild(scrollNode)
     }
     
-    func setKi(){ //画面外に木を設置
-        //豆の木の画像を読み込む
-        let kiTexture = SKTexture(imageNamed: "mamenoki")
-        kiTexture.filteringMode = SKTextureFilteringMode.nearest
-        
-        //スプライトを作成
-        ki2 = SKSpriteNode(texture: kiTexture)
-        
-        
-        //スプライトの位置を指定する
-        ki2.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2 - kiTexture.size().height * 0.5)
-        ki2.zPosition = -100
-        
-        //物理演算を設定
-        ki2.physicsBody = SKPhysicsBody(rectangleOf: kiTexture.size(), center: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height))
-        //質量を設定(葉と同じ)
-        ki2.physicsBody?.mass = 0.07
-        
-        ki2.physicsBody?.allowsRotation = false //回転しないようにする
-        
-        //次の画面を用意するための判定
-        scrollNode2 = SKNode()
-        scrollNode2.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
-        scrollNode2.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.width * 2, height: 1.0))
-        scrollNode2.physicsBody?.categoryBitMask = self.scrollCategory
-        scrollNode2.physicsBody?.contactTestBitMask = self.mameCategory
-        scrollNode2.physicsBody?.mass = 0.07
-        
-        
-        
-        //シーンにスプライトを追加
-        addChild(ki2)
-        addChild(scrollNode2)
-    }
+
     
     
     func mameJump(){
@@ -255,23 +248,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.mame?.run(goback)
         
         //スクロールの速度をゼロにする
-        ki.physicsBody?.velocity = CGVector.zero
+        backScreenNode.physicsBody?.velocity = CGVector.zero
 
         
-        ki.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: -30.0))
+        backScreenNode.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: -30.0))
 
     }
     
     
     //SKPhysicsContactDelegateのメソッド。衝突した時に呼ばれる
     func didBegin(_ contact: SKPhysicsContact) {
-        print("contact")
-        
+        let centering = SKAction.moveTo(x: self.frame.size.width / 2, duration: 0.0)
+        backScreenNode.run(centering)
+
         if (contact.bodyA.categoryBitMask & leafCategory) == leafCategory || (contact.bodyB.categoryBitMask & leafCategory) == leafCategory { //まめくんが葉と衝突した時
-            print("jump")
+            print("contact:leaf")
             mameJump()
         }else if (contact.bodyA.categoryBitMask & scrollCategory) == scrollCategory || (contact.bodyB.categoryBitMask & scrollCategory) == scrollCategory { //まめくんがスクロール判定を通過した時
-            print("scroll")
+            print("contact:scroll")
             if (contact.bodyA.categoryBitMask & scrollCategory) == scrollCategory{ //スクロール判定を消す
                 contact.bodyA.node?.removeFromParent()
             }else{
